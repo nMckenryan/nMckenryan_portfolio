a17634fd55c5bafd69b6026ef6f08d570a8d2cf3
nMckenryan <n.mckenryan@gmail.com>
2024-10-11 13:12:19 +1100

    Implemented basic hero section start and simplified sidebar

--- src/components/SideHeaderBar.tsx
+++ src/components/SideHeaderBar.tsx
@@ -1,7 +1,7 @@
 import { useRef, type ReactElement } from 'react'
 
-import { Scrollspy } from '@makotot/ghostui'
 import avatar from '../../public/apple-touch-icon.png'
+import HeroSection from './Section'
 
 export default function Sidebar(): ReactElement {
 	const tabs = [
@@ -22,70 +22,53 @@ export default function Sidebar(): ReactElement {
 		useRef<HTMLDivElement>(null)
 	]
 	return (
-		<Scrollspy sectionRefs={sectionReferences}>
-			{({ currentElementIndexInViewport }) => (
-				// LEFT HAND BAR
-				<div
-					id='sidebar'
-					className='h-screen flex-row items-center justify-center '
-				>
-					<ul
-						data-cy='nav-wrapper'
-						className='md:position-fixed fixed hidden h-full  w-1/6 list-none flex-col items-center justify-center bg-blue-900 md:flex'
-					>
-						<div>
-							<img
-								className='mx-auto mb-2 size-32 rounded-full drop-shadow-lg'
-								src={avatar}
-								alt='avatar'
-							/>
-						</div>
-						{tabs.map((item, index) => (
-							<li
-								key={item.href}
-								className={
-									currentElementIndexInViewport === index ? 'active' : ''
-								}
-							>
-								<a
-									href={`#section-${index}`}
-									className={`text-2xl font-bold ${
-										currentElementIndexInViewport === index
-											? 'text-white'
-											: 'text-gray-400'
-									}`}
-								>
-									{item.content}
-								</a>
-							</li>
-						))}
-					</ul>
-					{/* RIGHT SIDE SECTION */}
-					<div data-cy='section-wrapper' className='flex flex-col'>
-						{tabs.map((_, index) => (
-							<div
-								id={`section-${index}`}
-								key='key'
-								ref={sectionReferences[index]}
-								className={
-									currentElementIndexInViewport === index ? 'active' : ''
-								}
-								style={{
-									display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center',
-									height: '500px',
-									backgroundColor: `#${index}${index}${index}`,
-									color: '#fff',
-									fontSize: '2rem'
-								}}
-							>
-								{index}
-							</div>
-						))}
-					</div>
+		<div
+			id='sidebar'
+			className='h-screen flex-row items-center justify-center '
+		>
+			<div
+				data-cy='nav-wrapper'
+				className='md:position-fixed fixed hidden h-full w-1/6  list-none flex-col items-center justify-center bg-blue-900 md:flex'
+			>
+				<div>
+					<img
+						className='mx-auto mb-2 size-32 rounded-full drop-shadow-lg'
+						src={avatar}
+						alt='avatar'
+					/>
 				</div>
-			)}
-		</Scrollspy>
+				<ul>
+					{tabs.map((item, index) => (
+						<li key={item.href}>
+							<a href={`#section-${index}`} className='text-2xl font-bold'>
+								{item.content}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+			{/* RIGHT SIDE SECTION */}
+			<div data-cy='section-wrapper' className='flex flex-col'>
+				<HeroSection />
+				{tabs.map((_, index) => (
+					<div
+						id={`section-${index}`}
+						key='key'
+						ref={sectionReferences[index]}
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							height: '500px',
+							backgroundColor: `#${index}${index}${index}`,
+							color: '#fff',
+							fontSize: '2rem'
+						}}
+					>
+						{index}
+					</div>
+				))}
+			</div>
+		</div>
 	)
 }
