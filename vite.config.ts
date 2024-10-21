d23a2e1ee6997d8935deaf88a3884fb32dbdd450
nMckenryan <n.mckenryan@gmail.com>
2024-10-22 01:28:44 +1100

    Build settings

--- vite.config.ts
+++ vite.config.ts
@@ -31,33 +31,34 @@ export default defineConfig(({ mode }) => ({
 		...(mode === 'test'
 			? []
 			: [
-				eslintPlugin(),
-				VitePWA({
-					registerType: 'autoUpdate',
-					includeAssets: [
-						'favicon.ico',
-						'robots.txt',
-						'apple-touch-icon.png',
-						'icons/*.svg',
-						'fonts/*.woff2'
-					],
-					manifest: {
-						theme_color: '#BD34FE',
-						icons: [
-							{
-								src: '/android-chrome-192x192.png',
-								sizes: '192x192',
-								type: 'image/png',
-								purpose: 'any maskable'
-							},
-							{
-								src: '/android-chrome-512x512.png',
-								sizes: '512x512',
-								type: 'image/png'
-							}
-						]
-					}
-				})
-			])
-	]
+					eslintPlugin(),
+					VitePWA({
+						registerType: 'autoUpdate',
+						includeAssets: [
+							'favicon.ico',
+							'robots.txt',
+							'apple-touch-icon.png',
+							'icons/*.svg',
+							'fonts/*.woff2'
+						],
+						manifest: {
+							theme_color: '#BD34FE',
+							icons: [
+								{
+									src: '/android-chrome-192x192.png',
+									sizes: '192x192',
+									type: 'image/png',
+									purpose: 'any maskable'
+								},
+								{
+									src: '/android-chrome-512x512.png',
+									sizes: '512x512',
+									type: 'image/png'
+								}
+							]
+						}
+					})
+				])
+	],
+	base: 'nmckenryan.github.io'
 }))
